641f46be34d40cd521ccbc8f0e086f78892f85c8
김현빈 <esansi970524@gmail.com>
2025-01-13 18:54:57 +0900

    last

---
+++ yyyy copy.js
@@ -0,0 +1,64 @@
+class Node {
+    constructor(data) {
+        // 노드 값
+        this.data = data;
+        // 다음 노드에 대한 참조를 저장
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        // 큐의 첫 번째 노드
+        this.head = null;
+        // 큐의 마지막 노드
+        this.tail = null;
+        // 큐의 길이
+        this.size = 0;
+    }
+
+    enqueue(data) {
+        // 새로운 노드 생성
+        const newNode = new Node(data);
+
+        // 큐가 비어있을 경우 head와 tail을 모두 새 노드로 설정
+        // 그렇지 않으면 현재 tail의 next 속성을 새 노드로 설정하고 tail이 새 노드를 가리키도록 업데이트
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+
+        this.size++;
+    }
+
+    dequeue() {
+        // 큐가 비어있을 경우 null 반환
+        // 그렇지 않으면 head를 큐의 두 번째 요소로 설정하고 제거된 노드의 data를 반환
+        if (!this.head) {
+            return null;
+        }
+
+        const removeNode = this.head;
+        this.head = this.head.next;
+        if (!this.head) {
+            this.tail = null;
+        }
+
+        this.size--;
+
+        return removeNode.data;
+    }
+    // 큐가 비어있을 경우 true 반환, 그렇지 않으면 false 반환 
+    isEmpty() {
+        return this.size === 0;
+    }
+}
+
+const Q = new Queue();
+Q.enqueue(1);
+Q.enqueue(2);
+Q.dequeue(3);
+console.log(Q)
--- yyyy.js
+++ yyyy.js
@@ -1,27 +1,45 @@
-class DoubleLinkedList {
-    constructor(id) {
+class SingleLinkedList {
+    constructor(id, v) {
         this.id = id;
-        this._value = "";
-        this.pNode = null;
-        this.nNode = null;
+        this._value = v;
+        this.nextNode = null;
     }
-    set value(value) { this._value = value; }
     get value() { return this._value; }
-    setPNode(node) { this.pNode = node; }
-    setNNode(node) { this.nNode = node; }
-    prevNode() { return this.pNode; }
-    nextNode() { return this.nNode; }
+    set value(item) { this._value = item; }
+    setNext(nextNode) { this.nextNode = nextNode; }
+    next() { return this.nextNode; }
 }
+class Queue {
+    constructor() {
+        // 큐의 첫 번째 노드
+        this.head = null;
+        // 큐의 마지막 노드
+        this.tail = null;
+        // 큐의 길이
+        this.size = 0;
+    }
+    enqueue(v) {
+        const newNode = new Node(v);
 
-const dll1 = new DoubleLinkedList("dll1");
-const dll2 = new DoubleLinkedList("dll2");
-const dll3 = new DoubleLinkedList("dll3");
-dll1.value = "Love";
-dll2.value = "CDR";
-dll3.value = "Zard";
-dll1.setNNode(dll2);
-dll2.setPNode(dll1);
-dll2.setNNode(dll3);
-dll3.setPNode(dll2);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
 
-console.log(dll1.nextNode().nextNode().prevNode().value);
+        this.size++;
+    }
+}
+const Q = new Queue();
+console.log(Q)
+
+
+const sLL1 = new SingleLinkedList("sLL1", "ZARD");
+const sLL2 = new SingleLinkedList("sLL2", "ZAR3");
+const sLL3 = new SingleLinkedList("sLL3", "ZA23");
+
+sLL1.setNext(sLL2);
+sLL2.setNext(sLL3);
+console.log(sLL1);
